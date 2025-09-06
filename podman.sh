13671d71ce8c76a878fbcf3dfe975328e7a5ce30
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2025-09-07 00:04:34 +0200

    feat(ws): fix

--- podman.sh
+++ podman.sh
@@ -4,5 +4,5 @@ podman build -t 192.168.0.10:5000/edumdns-image .
 #podman push cerit.io/roman_alexander_mariancik/edumdns-image:latest
 podman push 192.168.0.10:5000/edumdns-image
 ssh roman@server systemctl --user restart edumdns.service
-ssh roman@hp systemctl --user restart edumdns.service
+# ssh roman@hp systemctl --user restart edumdns.service
 #kubectl apply -f kubernetes/edumdns -n mariancik-ns

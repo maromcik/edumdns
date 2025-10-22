28247982dd545630726feff945074ee3a4e8ebc4
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2025-10-22 21:34:02 +0200

    feat(db): add packet pipelining

--- edumdns_server/src/database.rs
+++ edumdns_server/src/database.rs
@@ -6,7 +6,10 @@ use edumdns_db::repositories::device::models::CreateDevice;
 use edumdns_db::repositories::device::repository::PgDeviceRepository;
 use edumdns_db::repositories::packet::models::CreatePacket;
 use edumdns_db::repositories::packet::repository::PgPacketRepository;
+use futures::stream::{FuturesUnordered, StreamExt};
 use log::{debug, error};
+use std::time::Duration;
+use tokio::time::sleep;
 
 pub enum DbCommand {
     StoreDevice(ProbePacket),
@@ -32,27 +35,40 @@ impl DatabaseManager {
     }
 
     pub async fn handle_database(&mut self) {
-        while let Some(command) = self.receiver.recv().await {
-            match command {
-                DbCommand::StoreDevice(device_packet) => {
-                    self.store_device_in_database(device_packet).await;
+        let mut futs = FuturesUnordered::new();
+        loop {
+            tokio::select! {
+                Some(command) = self.receiver.recv() => {
+                                match command {
+                                    DbCommand::StoreDevice(device_packet) => {
+                                        Self::store_device_in_database(&self.device_repo, device_packet).await;
+                                    }
+                                    DbCommand::StorePacket(probe_packet) => {
+                                        futs.push(Self::store_packet_in_database(&self.packet_repo, probe_packet));
+                                        if futs.len() > 10 {
+                                            futs.next().await;
+                                        }
+                                    }
+                                }
                 }
-                DbCommand::StorePacket(probe_packet) => {
-                    self.store_packet_in_database(probe_packet).await;
+                ret = futs.next() => {
+                    if ret.is_none() {
+                            sleep(Duration::from_millis(50)).await;
+                        }
                 }
+
             }
         }
     }
 
-    async fn store_device_in_database(&self, packet: ProbePacket) {
+    async fn store_device_in_database(device_repo: &PgDeviceRepository, packet: ProbePacket) {
         let src_mac = packet
             .packet_metadata
             .datalink_metadata
             .mac_metadata
             .src_mac;
         let src_ip = packet.packet_metadata.ip_metadata.src_ip;
-        let device = self
-            .device_repo
+        let device = device_repo
             .create(&CreateDevice::new_discover(
                 packet.probe_metadata.id.0,
                 src_mac.to_octets(),
@@ -78,15 +94,14 @@ impl DatabaseManager {
         };
     }
 
-    pub async fn store_packet_in_database(&self, packet: ProbePacket) {
+    pub async fn store_packet_in_database(packet_repo: &PgPacketRepository, packet: ProbePacket) {
         let src_mac = packet
             .packet_metadata
             .datalink_metadata
             .mac_metadata
             .src_mac;
         let src_ip = packet.packet_metadata.ip_metadata.src_ip;
-        let packet = self
-            .packet_repo
+        let packet = packet_repo
             .create(&CreatePacket::new(
                 packet.probe_metadata.id.0,
                 src_mac.to_octets(),
